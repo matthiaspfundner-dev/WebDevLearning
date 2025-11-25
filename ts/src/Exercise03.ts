896e669aef5b62bd30db09367f3bf6e7a72d7e3b
matthiaspfundner <matthiaspfundner@gmail.com>
2025-11-25 11:07:32 +0100

    Add short exercises to Exercise03.ts

--- ts/src/Exercise03.ts
+++ ts/src/Exercise03.ts
@@ -1,4 +1,5 @@
-﻿class Student {
+﻿//#region Exercise 01
+class Student {
     name: string
     age: number
     isStudent: boolean
@@ -12,3 +13,60 @@
 
 let student = new Student("John Doe", 20, true)
 console.log(`Name: ${student.name}, Age: ${student.age}, Is Student: ${student.isStudent}`)
+
+
+//#endregion
+
+//#region Exercise 02
+interface PersonExercise02 {
+    name: string
+    age: number
+    isStudent: boolean
+}
+
+let personExercise02: PersonExercise02 = {
+    name: "Max",
+    age: 25,
+    isStudent: true
+}
+
+console.log(`Name: ${personExercise02.name}, Age: ${personExercise02.age}, Is Student: ${personExercise02.isStudent}`)
+//#endregion
+
+//#region Exercise 03
+interface PersonExercise03 {
+    name: string
+    age: number
+    isStudent?: boolean
+}
+
+let personExercise03WithIsStudent: PersonExercise03 = {
+    name: "Max",
+    age: 25,
+    isStudent: true
+}
+
+let personExercise03WithoutIsStudent: PersonExercise03 = {
+    name: "Max",
+    age: 25
+}
+
+console.log(`Name: ${personExercise03WithIsStudent.name}, Age: ${personExercise03WithIsStudent.age}, Is Student: ${personExercise03WithIsStudent.isStudent}`)
+console.log(`Name: ${personExercise03WithoutIsStudent.name}, Age: ${personExercise03WithoutIsStudent.age}, Is Student: ${personExercise03WithoutIsStudent.isStudent}`)
+
+//#endregion
+
+//#region Exercise 04
+class Car {
+    make: number
+    model: string
+
+    constructor(make: number, model: string) {
+        this.make = make
+        this.model = model
+    }
+}
+
+let car = new Car(1999, "Audi")
+console.log(`${car.make}, ${car.model} `)
+//#endregion
